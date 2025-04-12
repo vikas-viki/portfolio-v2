a6a81830606684fd15a4b9b39957c845b13489fe
vikas-viki <viki57394@gmail.com>
2025-04-12 18:09:41 +0530

    x handle update

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -12,9 +12,9 @@ const Contact = () => {
                     <div className="handles-me flex gap-4 w-full h-full customShadow">
                         <div className="text font-outfit border border-slate-400 p-4 rounded-[5px]  bg-slate-900">
                             <span className="block text-outfit font-semibold text-slate-100 text-[25px] mb-2">About me</span>
-                            <span className="text-slate-300 text-[20px]">Hello, I'm Vikas Kotary. I enjoy learning new things and solving problems at their root to build immersive digital applications, whether in frontend, backend or smart contract development. I also love listening to music. <br/><span className="block mt-2 text-slate-200">Every day we learn something - notice it and make it count - Me.</span></span>
+                            <span className="text-slate-300 text-[20px]">Hello, I'm Vikas Kotary. I enjoy learning new things and solving problems at their root to build immersive digital applications, whether in frontend, backend or smart contract development. I also love listening to music. <br /><span className="block mt-2 text-slate-200">Every day we learn something - notice it and make it count - Me.</span></span>
                             <div className="px-4 pt-2 rounded-[5px] flex gap-4 w-full flex-wrap  justify-end items-center">
-                                <a target="blank" rel="noopener noreferrer" href="https://x.com/0x_viki"><Twitter className="link cursor-pointer"/></a>
+                                <a target="blank" rel="noopener noreferrer" href="https://x.com/devdoodvikas"><Twitter className="link cursor-pointer" /></a>
                                 <a target="blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/vikaskotary/"><Linkedin className="link cursor-pointer" /></a>
                                 <a target="blank" href="mailto:vikaskotary001@outlook.com"><Mail className="link cursor-pointer" /></a>
                             </div>
